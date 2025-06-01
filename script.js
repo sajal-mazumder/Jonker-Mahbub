99045bcc85d08219d91b8c2d04b84a4fd30e362b
sajal-mazumder <sajalmazumder47@gmail.com>
2025-06-01 12:16:05 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,130 @@
+
+
+// $$$ Chapter 2 : topics : variable
+
+// 1. my birthYear
+
+let myBirthYear = 1991;
+console.log(myBirthYear);
+
+// 2. my brother and sisters
+
+let brothersSisters = 4;
+console.log(brothersSisters);
+
+// 3. class students
+
+let classStudents = 65;
+console.log(classStudents);
+
+// 4. last exam marks
+
+let examMarks = 756;
+console.log(examMarks);
+
+// 5. house rooms
+
+var rooms = 5;
+console.log(rooms);
+
+// 6. study time
+
+const studyTime = 6;
+console.log(studyTime);
+
+// 7. mobile number digit sum
+
+let digitSum = 32;
+console.log(digitSum);
+
+// ### topics : console log
+
+// 1. current age 
+
+let age = 33;
+console.log(age);
+
+// 2. total books read
+
+let bookToRead = 4;
+console.log(bookToRead);
+
+// 3. wake up from bed
+
+var wakeUpTime = 7;
+console.log(wakeUpTime);
+
+// 4. mobile waste time
+
+const wasteTime = 5;
+console.log(wasteTime);
+
+// 5. borrow money
+
+var borrow = 4400;
+console.log(borrow);
+
+// 6. favorite team goal
+
+var goals = 3;
+console.log(goals);
+
+// 7. pillow number in bed
+
+const pillows = 4;
+console.log(pillows);
+
+// ### topics : string
+
+// 1. my name
+
+let name = "sajal";
+console.log(name);
+
+// 2. my address
+
+var address = 'chattagram';
+console.log(address);
+
+// 3. friends versity
+
+const varsity = 'Dhaka University';
+console.log(varsity);
+
+// 4. favorite heroine
+
+let heroine = 'purnima';
+console.log(heroine);
+
+// 5. favorite movie
+
+let favMovie = 'don';
+console.log(favMovie);
+
+// 6. my hobby
+
+let myHobby = 'gardening';
+console.log(myHobby);
+
+// 7. favorite color 
+
+const favColor = 'black';
+console.log(favColor);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
