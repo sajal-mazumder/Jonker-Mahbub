8129729dd92cb0d035d3fc89b594ccad4133ee2d
sajal-mazumder <sajalmazumder47@gmail.com>
2025-06-01 13:35:15 +0600

    some update

--- script.js
+++ script.js
@@ -4,242 +4,327 @@
 
 // 1. my birthYear
 
-let myBirthYear = 1991;
-console.log(myBirthYear);
+// let myBirthYear = 1991;
+// console.log(myBirthYear);
 
-// 2. my brother and sisters
+// // 2. my brother and sisters
 
-let brothersSisters = 4;
-console.log(brothersSisters);
+// let brothersSisters = 4;
+// console.log(brothersSisters);
 
-// 3. class students
+// // 3. class students
 
-let classStudents = 65;
-console.log(classStudents);
+// let classStudents = 65;
+// console.log(classStudents);
 
-// 4. last exam marks
+// // 4. last exam marks
 
-let examMarks = 756;
-console.log(examMarks);
+// let examMarks = 756;
+// console.log(examMarks);
 
-// 5. house rooms
+// // 5. house rooms
 
-var rooms = 5;
-console.log(rooms);
+// var rooms = 5;
+// console.log(rooms);
 
-// 6. study time
+// // 6. study time
 
-const studyTime = 6;
-console.log(studyTime);
+// const studyTime = 6;
+// console.log(studyTime);
 
-// 7. mobile number digit sum
+// // 7. mobile number digit sum
 
-let digitSum = 32;
-console.log(digitSum);
+// let digitSum = 32;
+// console.log(digitSum);
 
-// ### topics : console log
+// // ### topics : console log
 
-// 1. current age 
+// // 1. current age 
 
-let age = 33;
-console.log(age);
+// let age = 33;
+// console.log(age);
 
-// 2. total books read
+// // 2. total books read
 
-let bookToRead = 4;
-console.log(bookToRead);
+// let bookToRead = 4;
+// console.log(bookToRead);
 
-// 3. wake up from bed
+// // 3. wake up from bed
 
-var wakeUpTime = 7;
-console.log(wakeUpTime);
+// var wakeUpTime = 7;
+// console.log(wakeUpTime);
 
-// 4. mobile waste time
+// // 4. mobile waste time
 
-const wasteTime = 5;
-console.log(wasteTime);
+// const wasteTime = 5;
+// console.log(wasteTime);
 
-// 5. borrow money
+// // 5. borrow money
 
-var borrow = 4400;
-console.log(borrow);
+// var borrow = 4400;
+// console.log(borrow);
 
-// 6. favorite team goal
+// // 6. favorite team goal
 
-var goals = 3;
-console.log(goals);
+// var goals = 3;
+// console.log(goals);
 
-// 7. pillow number in bed
+// // 7. pillow number in bed
 
-const pillows = 4;
-console.log(pillows);
+// const pillows = 4;
+// console.log(pillows);
 
-// ### topics : string
+// // ### topics : string
 
-// 1. my name
+// // 1. my name
 
-let name = "sajal";
-console.log(name);
+// let name = "sajal";
+// console.log(name);
 
-// 2. my address
+// // 2. my address
 
-var address = 'chattagram';
-console.log(address);
+// var address = 'chattagram';
+// console.log(address);
 
-// 3. friends versity
+// // 3. friends versity
 
-const varsity = 'Dhaka University';
-console.log(varsity);
+// const varsity = 'Dhaka University';
+// console.log(varsity);
 
-// 4. favorite heroine
+// // 4. favorite heroine
 
-let heroine = 'purnima';
-console.log(heroine);
+// let heroine = 'purnima';
+// console.log(heroine);
 
-// 5. favorite movie
+// // 5. favorite movie
 
-let favMovie = 'don';
-console.log(favMovie);
+// let favMovie = 'don';
+// console.log(favMovie);
 
-// 6. my hobby
+// // 6. my hobby
 
-let myHobby = 'gardening';
-console.log(myHobby);
+// let myHobby = 'gardening';
+// console.log(myHobby);
 
-// 7. favorite color 
+// // 7. favorite color 
 
-const favColor = 'black';
-console.log(favColor);
+// const favColor = 'black';
+// console.log(favColor);
 
 
-// ### topics : boolean
+// // ### topics : boolean
 
-// 1 . is happy
+// // 1 . is happy
 
-let happy = false;
-console.log(happy);
+// let happy = false;
+// console.log(happy);
 
-// 2. crush message
+// // 2. crush message
 
-let crushMassege = true;
-console.log(crushMassege);
+// let crushMassege = true;
+// console.log(crushMassege);
 
-// 3. team win celebrating
+// // 3. team win celebrating
 
-let celebrating = true;
-console.log(celebrating);
+// let celebrating = true;
+// console.log(celebrating);
 
-// 4. three declaration
+// // 4. three declaration
 
-let isHappy = true;
-console.log(isHappy);
+// let isHappy = true;
+// console.log(isHappy);
 
-let hasMoney = false;
-console.log(hasMoney);
+// let hasMoney = false;
+// console.log(hasMoney);
 
-let isHungry = true;
-console.log(isHungry);
+// let isHungry = true;
+// console.log(isHungry);
 
-// 5. is study done
+// // 5. is study done
 
-const isStudyDone = true;
-console.log(isStudyDone);
+// const isStudyDone = true;
+// console.log(isStudyDone);
 
-// 6. phone charged
+// // 6. phone charged
 
-let isPhoneCharged = true;
-console.log(isPhoneCharged);
+// let isPhoneCharged = true;
+// console.log(isPhoneCharged);
 
-// 7. dinner done
+// // 7. dinner done
 
-let isDinnerDone = false;
-console.log(isDinnerDone);
+// let isDinnerDone = false;
+// console.log(isDinnerDone);
 
-// ### js math
+// // ### js math
 
-// 1. math 1 substraction
+// // 1. math 1 substraction
 
-let earn = 100;
-let cost = 40;
-let rest = earn - cost ;
-console.log(rest);
+// let earn = 100;
+// let cost = 40;
+// let rest = earn - cost ;
+// console.log(rest);
 
-// 2. math 2 division
+// // 2. math 2 division
 
-let myPencil = 10;
-let perUse = 2;
-let totalUse = myPencil/ perUse;
-console.log(totalUse);
+// let myPencil = 10;
+// let perUse = 2;
+// let totalUse = myPencil/ perUse;
+// console.log(totalUse);
 
-// 3. math 3 division and modulus
+// // 3. math 3 division and modulus
 
-let myTaka = 60;
-let perCost = 15;
-let totalAttempt = myTaka / perCost;
-console.log(totalAttempt); // division
+// let myTaka = 60;
+// let perCost = 15;
+// let totalAttempt = myTaka / perCost;
+// console.log(totalAttempt); // division
 
-let totalRest = myTaka % perCost
-console.log(totalRest);  // modulus
+// let totalRest = myTaka % perCost
+// console.log(totalRest);  // modulus
 
-// 4. math 4 concat string
+// // 4. math 4 concat string
 
-let hello = 'Hello';
-let world = "World";
-let concat = hello + world;
-console.log(concat);
+// let hello = 'Hello';
+// let world = "World";
+// let concat = hello + world;
+// console.log(concat);
 
-// 5. math 5 addition
+// // 5. math 5 addition
 
-let rice = 153;
-let dal = 261;
-let total = rice + dal;
-console.log(total);
+// let rice = 153;
+// let dal = 261;
+// let total = rice + dal;
+// console.log(total);
 
-// 6. math 6 division and modulus
+// // 6. math 6 division and modulus
 
-let myMoney = 500;
-let canCost = 75;
-let howMany = myMoney / canCost;
-console.log(howMany);
+// let myMoney = 500;
+// let canCost = 75;
+// let howMany = myMoney / canCost;
+// console.log(howMany);
 
-let moneyRest = myMoney % canCost;
-console.log(moneyRest);
+// let moneyRest = myMoney % canCost;
+// console.log(moneyRest);
 
-// 7. math 7 division and modulus
+// // 7. math 7 division and modulus
 
-const number = 8;
-const number2 = 3;
-const numResult = number / number2;
-console.log(numResult);
+// const number = 8;
+// const number2 = 3;
+// const numResult = number / number2;
+// console.log(numResult);
 
-const numRst = number % number2;
-console.log(numRst);
+// const numRst = number % number2;
+// console.log(numRst);
 
-// 8. math 8 only modulus
+// // 8. math 8 only modulus
 
-let fifty = 50;
-let nine = 9;
-let modulus = fifty / nine;
-console.log(modulus);
+// let fifty = 50;
+// let nine = 9;
+// let modulus = fifty / nine;
+// console.log(modulus);
 
-// 9. math 9 concat string
+// // 9. math 9 concat string
 
-let bangla = 'Bangla';
-let desh = 'desh';
-let bangladesh = bangla + desh;
-console.log(bangladesh);
+// let bangla = 'Bangla';
+// let desh = 'desh';
+// let bangladesh = bangla + desh;
+// console.log(bangladesh);
 
-// 10. math 10 
+// // 10. math 10 
 
-var rices = 12;
-var riceKg = 12* 40;
-var dailyEat = 4;
-var eatDay = riceKg / dailyEat;
-console.log(eatDay);
+// var rices = 12;
+// var riceKg = 12* 40;
+// var dailyEat = 4;
+// var eatDay = riceKg / dailyEat;
+// console.log(eatDay);
 
-var month = 30;
-var eatMonth = riceKg / month;
-console.log(eatMonth);
+// var month = 30;
+// var eatMonth = riceKg / month;
+// console.log(eatMonth);
+
+// // @@@ chapter 3: data type
+// // topics : type of 
+
+// // 1. type of price
+
+// let price = 555;
+// console.log(typeof price);
+
+// // 2. type of true
+
+// let learning = true;
+// console.log(typeof learning);
+
+// // 3. type of megacity
+
+// let megaCity = 'Dhaka';
+// console.log(megaCity);
+
+// // 4. type of temperature
+
+// var temp = 102.5;
+// console.log(typeof temp);
+
+// // 5. type of boolean
+
+// let isHungryy = false;
+// console.log(typeof isHungryy);
+
+// // 6. is rich typeof
+
+// let isRich = true;
+// console.log(typeof isRich);
+
+// topics : variable change
+
+// 1. 
+let myAge = 30;
+myAge = 33;
+console.log(myAge);
+
+// 2. 
+
+let pocketMoney = 500;
+pocketMoney = 200;
+console.log(pocketMoney);
+
+// 3. 
+
+let studyTimes = 4;
+studyTimes = 5;
+console.log(studyTimes);
+
+// 4. 
+
+let friendsCount = 10;
+friendsCount = 10 - 2;
+console.log(friendsCount);
+
+// 5. 
+
+let phonePrice = 25000;
+phonePrice = 12500;
+console.log(phonePrice);
+
+// 6. 
+
+let chairsCount = 40;
+chairsCount = 40-2;
+chairsCount = (40 - 2)- 6;
+console.log(chairsCount);
+
+// 7. 
+
+let playTime = 3;
+playTime = 0;
+console.log(playTime);
+
+
+
+
+
+
+
+
 
 
 
